386657d5fe5927381c973263109429585afb83ec
Aunarky <54250170+Aunarky@users.noreply.github.com>
2025-10-21 23:47:48 +0100

    some extra changes to main menu (fixed Wait)

--- glasgo-ware/scenes/transition.gd
+++ glasgo-ware/scenes/transition.gd
@@ -5,7 +5,7 @@ extends CanvasLayer
 func fade_to_scene(path: String):
 	anim_player.play("fade_out")
 	await anim_player.animation_finished
-	Global.wait(1)
+	await Global.wait(.15)
 	get_tree().change_scene_to_file(path)
 	await get_tree().scene_changed
 	anim_player.play("fade_in")
