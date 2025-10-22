1c7b16f3229ae920a8b01a79c59ce36335b18657
Aunarky <54250170+Aunarky@users.noreply.github.com>
2025-10-22 18:30:15 +0100

    settings

--- glasgo-ware/scenes/titlescreen/titlescreen.gd
+++ glasgo-ware/scenes/titlescreen/titlescreen.gd
@@ -1,17 +1,35 @@
 extends Control
 
-@onready var regular_button: Button = $Buttons/Endless
+var settings_scene: PackedScene = preload("res://scenes/titlescreen/settingspage.tscn")
+
+@onready var buttons: VBoxContainer = $Buttons
+@onready var regular_button: Button = $Buttons/Regular
 @onready var endless_button: Button = $Buttons/Endless
 @onready var settings_button: Button = $Buttons/Settings
 
 func _ready():
 	regular_button.pressed.connect(_regular_play_button)
 	endless_button.pressed.connect(_endless_play_button)
+	settings_button.pressed.connect(_settings_spawn)
 
 func _regular_play_button():
+	_disable_buttons()
 	Global.game_type = Constants.GAME_TYPE.REGULAR
 	Transition.fade_to_scene("res://scenes/elevator/elevator.tscn")
 
 func _endless_play_button():
+	_disable_buttons()
 	Global.game_type = Constants.GAME_TYPE.ENDLESS
 	Transition.fade_to_scene("res://scenes/elevator/elevator.tscn")
+	
+func _settings_spawn():
+	_disable_buttons()
+	
+	var active_settings_scene = settings_scene.instantiate()
+	active_settings_scene.closed.connect(func(): _disable_buttons(false))
+	add_child(active_settings_scene)
+
+func _disable_buttons(disable: bool = true):
+	for b in buttons.get_children():
+		if b is Button:
+			b.disabled = disable
