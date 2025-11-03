c0e54cd9452f53a8325e884002937628f202cb97
Acul4321 <124876210+Acul4321@users.noreply.github.com>
2025-11-03 16:26:39 +0000

    Deploying to gh-pages from @ Acul4321/850-glasgow-game-jam@9b959674f12064f7dbca238305d8b0b6929bdeca 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1762184965|5660074';
+const CACHE_VERSION = '1762187189|6503536';
 /** @type {string} */
 const CACHE_PREFIX = 'GlasgoWare-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
